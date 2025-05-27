32caf308a3030064ea927b4be53e6cedbf6c0498
orejuelawilton <orejuelawilton@gmail.com>
2025-05-27 04:58:32 -0500

    mejoras

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -51,12 +51,10 @@ android {
     // Configuración para evitar conflictos de arquitectura
     splits {
         abi {
-            isEnable = true
-            reset()
-            include("armeabi-v7a", "arm64-v8a", "x86_64")
-            isUniversalApk = false
+        enable false
         }
     }
+
 }
 
 flutter {
